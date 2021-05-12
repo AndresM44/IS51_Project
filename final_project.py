7bb2550a4df7998c4bf092180ca4b50868033ab2
Andres Martinez <andres_m559@Andress-MacBook-Pro.local>
2021-05-11 22:10:29 -0700

    finished structured-english

---
+++ final_project.py
@@ -0,0 +1,24 @@
+
+
+"""
+This program is a Body Mass Index (BMI) calculator.
+This calculator is used to measure the body fat of a person based on their height and weight.
+The user will input their height (in centimeters) and the weight (in kilograms).
+The program will calculate whether they are underweight, healthy, overweight, extremely overweight, obese or extremely obese.
+
+BMI will be set to: weight / (height/100)**2(squared)
+
+A BMI of <= 18.4
+    print("You are underweight.")
+A BMI of <= 24.9
+    print("You are healthy.")
+A BMI of <= 29.9
+    print("You are overweight.")
+A BMI of <= 34.9
+    print("You are extremely overweight.")
+A BMI of <= 39.9
+    print("You are obese.")
+A BMI of => 39.9
+    print("You are extremely obese.")
+    
+"""
