dc290cca49cfc361eb8d39f60d64da7e4cf56f2c
Andres Martinez <andres_m559@Andress-MacBook-Pro.local>
2021-05-11 22:28:24 -0700

    finished main

--- final_project.py
+++ final_project.py
@@ -55,3 +55,32 @@ else:
     print("You are extremely obese.")
 
 """
+
+try:
+    height = float(input("Enter your height in CM: "))
+except NameError:
+    print("\nYou failed to input a number.")
+    print("\nPlease don't do this again.")
+except ZeroDivisionError:
+    print("\nOne of your inputs was a 0. That's impossible.")
+try:
+    weight = float(input("Enter your weight in KG: "))
+except NameError:
+    print("\nYou did it again...")
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is: ", BMI)
+
+if BMI <= 18.4:
+    print("You are underweight.")
+elif BMI <= 24.9:
+    print("You are healthy.")
+elif BMI <= 29.9:
+    print("You are overweight.")
+elif BMI <= 34.9:
+    print("You are extremely overweight.")
+elif BMI <= 39.9:
+    print("You are obese.")
+else:
+    print("You are extremely obese.")
